85b1390ea731ea57c64704ae5c5152946fcb2889
Louis Moore <burntliketoast@gmail.com>
2015-05-15 15:45:14 +1000

    Sinatra mounted to rails app -Load path updated to autoload  /lib

---
+++ app/models/lol.rb
@@ -0,0 +1,29 @@
+require 'sinatra/base'
+
+
+class Test2
+end
+
+class TestAPI # < Sinatra::Base
+
+# get '/' do
+#   erb :index
+# end
+
+# get '/api/tickets' do  
+#   Ticket.all.to_json
+# end
+
+
+# get '/api/talks' do  
+#   Talk.all.to_json
+# end
+
+# get '/api/speakers' do  
+#   Speaker.all.to_json
+# end
+
+end
+
+class Test4
+  end
