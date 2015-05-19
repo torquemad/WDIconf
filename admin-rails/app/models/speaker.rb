4c200ff081f0c6c4f6edadc88aaa39f00add45c4
JC Lee <jcleefw@gmail.com>
2015-05-20 02:03:23 +1000

    Make changes to speaker avatar photos, Speaker show page. Added gemfile and seed

--- admin-rails/app/models/speaker.rb
+++ admin-rails/app/models/speaker.rb
@@ -1,4 +1,7 @@
 class Speaker < ActiveRecord::Base
   has_many :talks
   belongs_to :user
+
+  mount_uploader :image_url, ImageUploader
+
 end
--- admin-rails/db/seeds.rb
+++ admin-rails/db/seeds.rb
@@ -9,9 +9,9 @@
 
 #  Users
 # id:serial, access_level_id: int, email: str, login_type:int
-User.create([{ access_level_id: 1, email: 'admin@uxb.com', login_type:1 }]);
-User.create([{ access_level_id: 3, email: 'dt@uxb.com', login_type:1 }]);
-User.create([{ access_level_id: 3, email: 'beta@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 1, email: 'admin@uxb.com', login_type:1, password: '123', password_salt: 'abcde' }]);
+User.create([{ access_level_id: 3, email: 'dt@uxb.com', login_type:1, password: '123', password_salt: 'abcde' }]);
+User.create([{ access_level_id: 3, email: 'beta@uxb.com', login_type:1, password: '123', password_salt: 'abcde' }]);
 
 
 #  Users Access Level
@@ -24,6 +24,7 @@ AccessLevel.create([{access_type:'sponsor'}]);
 # Talks
 # id: serial, title: str, blurb: str, description: text, timedate: datetime, limit: int, duration: int (mins), speaker_id: int
 Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 1 }]);
+Talk.create([{ title: 'Flipping Hair 2', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 1 }]);
 
 
 # Tickets
@@ -32,6 +33,6 @@ Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', descripti
 
 
 # Speakers
-# id: serial, user_id: int, name: str, blurb: str
-Speaker.create([{ user_id: 2, name: 'Daniel Tsui', blurb: 'Lead Instructor, Web Development Immersive, General Assembly'}]);
-Speaker.create([{ user_id: 3, name: 'Beta', blurb: 'Master of all UXB'}]);
+# id: serial, user_id: int, name: str, blurb: str, image_url: str
+Speaker.create([{ user_id: 2, name: 'Daniel Tsui', image_url: 'https://ga-core.s3.amazonaws.com/production/uploads/instructor/image/1700/thumb_HEADSHOTS__2001_.jpg', blurb: 'Lead Instructor, Web Development Immersive, General Assembly'}]);
+Speaker.create([{ user_id: 3, name: 'Beta', blurb: 'Master of all UXB', image_url: 'http://m.c.lnkd.licdn.com/media/p/8/005/06d/30d/2881546.jpg'}]);
