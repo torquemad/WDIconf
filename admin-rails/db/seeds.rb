f6f7826c2b5d59d2e1d124c34fe3b522b4a6d4e0
Luke Shillabeer <lshillabeer@gmail.com>
2015-05-19 18:25:58 +1000

    Added seed data and minor edit to Sinatra API

--- admin-rails/db/seeds.rb
+++ admin-rails/db/seeds.rb
@@ -1,18 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+# SEED DATA FOR DB
 
 #  Users
 # id:serial, access_level_id: int, email: str, login_type:int
 User.create([{ access_level_id: 1, email: 'admin@uxb.com', login_type:1 }]);
-User.create([{ access_level_id: 3, email: 'dt@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'aden@uxb.com', login_type:1 }]);
 User.create([{ access_level_id: 3, email: 'beta@uxb.com', login_type:1 }]);
-
+User.create([{ access_level_id: 3, email: 'dom@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'jared@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'jaya@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'jc@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'jeff@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'justin@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'louis@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'luke@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'matt@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'mike@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'nick@uxb.com', login_type:1 }]);
+User.create([{ access_level_id: 3, email: 'philip@uxb.com', login_type:1 }]);
 
 #  Users Access Level
 # id:serial, access_type: str
@@ -24,14 +28,40 @@ AccessLevel.create([{access_type:'sponsor'}]);
 # Talks
 # id: serial, title: str, blurb: str, description: text, timedate: datetime, limit: int, duration: int (mins), speaker_id: int
 Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 1 }]);
-
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 2 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 3 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 4 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 5 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 6 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 7 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 8 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 9 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 10 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 11 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 12 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 13 }]);
+Talk.create([{ title: 'Flipping Hair', blurb: 'How to flip your hair', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam sint ea debitis natus quos iusto earum corporis excepturi inventore pariatur ipsum non est itaque repellat, ratione aliquam accusantium placeat.', date: '', limit: 30, duration: 60, speaker_id: 14 }]);
 
 # Tickets
 # id: serial, registra: str, talk_id: int
-
-
+Ticket.create([{ :registra => "test1@test.com", :talk_id => 1 }])
+Ticket.create([{ :registra => "test1@test.com", :talk_id => 2 }])
+Ticket.create([{ :registra => "test1@test.com", :talk_id => 3 }])
+Ticket.create([{ :registra => "test2@test.com", :talk_id => 6 }])
 
 # Speakers
 # id: serial, user_id: int, name: str, blurb: str
-Speaker.create([{ user_id: 2, name: 'Daniel Tsui', blurb: 'Lead Instructor, Web Development Immersive, General Assembly'}]);
-Speaker.create([{ user_id: 3, name: 'Beta', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 2, name: 'Aden Margheriti', blurb: 'Eater of pizza, consumer of basketball'}]);
+Speaker.create([{ user_id: 3, name: 'Beta Ngadiman', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 4, name: 'Dominic White', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 5, name: 'Jared O\'Loughlin', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 6, name: 'Jaya Wijono', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 7, name: 'JC Lee', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 8, name: 'Jeff Wolff', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 9, name: 'Justin Ward', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 10, name: 'Louis Moore', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 11, name: 'Luke Shillabeer', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 12, name: 'Matt Cameron', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 13, name: 'Mike Goodwin', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 14, name: 'Nick Pateman', blurb: 'Master of all UXB'}]);
+Speaker.create([{ user_id: 15, name: 'Philip Castiglione', blurb: 'Master of all UXB'}]);
