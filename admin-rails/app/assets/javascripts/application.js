967f74e67d1085ebcc4a8b2bf87dab6a979369c7
JC Lee <jcleefw@gmail.com>
2015-05-21 17:32:21 +1000

    fix schedule dragging issues

--- admin-rails/app/assets/javascripts/application.js
+++ admin-rails/app/assets/javascripts/application.js
@@ -14,3 +14,49 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+//$(function() {
+var talk_draggable = function() {
+  $('.dropTarget').droppable({
+    drop: function(ev, ui) {
+        $(this)
+          .html( $(this).text().trim() + ($(ui.draggable).html()) )
+          .attr( "class", "inline-block" );
+        $(this).droppable('disable');
+        //
+        var cell = $( $(this).parent()[0] );
+        var cell_time = cell.find('.cell-time').text().trim().split(':');
+        // this is debug code to help with AJAX call
+        var url = ($(ui.draggable).find('a').attr('href')).split('/');
+        $.ajax({
+          url: "/api/talks/time/" + url[2],
+          method: "PATCH",
+          data: {
+            hour: cell_time[0],
+            minutes: cell_time[1],
+          }
+        });
+        $(ui.draggable).remove();
+        $(this).find('.talk-box-close').css("display","block");
+
+        // remove the data from the cell via X in top-right
+        $(this).on('click', '.talk-box-close', function(){
+          debugger;
+          //$removed = $(this).parent().appendTo('#talk-list').addClass("single-talk");
+          $removed =  $(this)
+                        .parent()
+                        .appendTo('#talk-list')
+                        .draggable()
+                        .addClass("single-talk");
+          $removeParent = $removed.parent();
+          $removeParent.droppable({disabled: false});
+          // $(this)
+          // .parent()
+          // .fadeOut("slow",function(){
+          //   $(this).remove();
+          // });
+        });
+      }
+  });
+}
+//});
