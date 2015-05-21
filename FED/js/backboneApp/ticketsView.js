a771e2f49b9a6118a07a8a4e7a3aadf27c60e0a0
Philip Castiglione <philipcastiglione@gmail.com>
2015-05-21 23:48:02 +1000

    started on tickets BB

---
+++ FED/js/backboneApp/ticketsView.js
@@ -0,0 +1,23 @@
+var TicketsView = Backbone.View.extend({
+  tagName: 'tr',
+
+  className: 'confirmationModalStyle',
+  
+  initialize: function(){
+    
+  },
+
+  events:{
+  },
+
+  render: function(){
+    // var ticketsTemplate = _.template( $('#tickets-template').html());
+    // var model = chosenTalks.toJSON();
+    // var title = model['title'];
+    // var speaker = model['speaker']['name'];
+    // var startTime = model['start_time'];
+    // var endTime = this.endTime(startTime);
+    // this.$el.html(talksTemplate({title: title, speaker: speaker, startTime: startTime, endTime: endTime}));
+    // return this;
+  }
+});
