472883f43827be1a862e1fe979e32b9948a231ef
JC Lee <jcleefw@gmail.com>
2015-05-21 13:34:11 +1000

    Make minor changes to rails features

--- admin-rails/db/schema.rb
+++ admin-rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520025755) do
+ActiveRecord::Schema.define(version: 20150521015940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20150520025755) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "identities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
 
   create_table "speakers", force: :cascade do |t|
     t.integer  "user_id"
@@ -42,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150520025755) do
     t.integer  "duration"
     t.integer  "speaker_id"
     t.string   "badge"
+    t.string   "start_time"
   end
 
   create_table "tickets", force: :cascade do |t|
